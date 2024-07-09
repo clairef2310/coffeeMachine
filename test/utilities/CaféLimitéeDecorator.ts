5b65999f893af3d3a59042eb3095055bd8b293a9
Fernandes Claire <claire.fernandes.ee@gmail.com>
2024-07-09 09:36:19 +0200

    ajout café decorator

---
+++ test/utilities/CaféLimitéeDecorator.ts
@@ -0,0 +1,75 @@
+import {ButtonCodes} from "../../src/hardware/hardware.interface";
+import {HardwareFake, HardwareFakeInterface} from "./HardwareFake";
+import {Pièce} from "../../src/Pièce";
+
+export class HardwareFakeDecorator implements HardwareFakeInterface {
+    protected _decorated: HardwareFakeInterface;
+
+    public constructor(decorated: HardwareFakeInterface) {
+        this._decorated = decorated;
+    }
+
+    RegisterMoneyInsertedCallback(callback: (coinValue: number) => void): void {
+        this._decorated.RegisterMoneyInsertedCallback(callback)
+    }
+    FlushStoredMoney(): void {
+        this._decorated.FlushStoredMoney()
+    }
+    CollectStoredMoney(): void {
+        this._decorated.CollectStoredMoney()
+    }
+    IsCupPresent(): boolean {
+        return this._decorated.IsCupPresent()
+    }
+    ProvideCup(): void {
+        return this._decorated.ProvideCup()
+    }
+    RegisterButtonPressedCallback(callback: (buttonCode: ButtonCodes) => void): void {
+        return this._decorated.RegisterButtonPressedCallback(callback)
+    }
+    MakeACoffee(): boolean {
+        return this._decorated.MakeACoffee()
+    }
+    TryPullWater(): boolean {
+        return this._decorated.TryPullWater()
+    }
+    PourMilk(): boolean {
+        return this._decorated.PourMilk()
+    }
+    PourWater(): boolean {
+        return this._decorated.PourWater()
+    }
+    PourSugar(): boolean {
+        return this._decorated.PourSugar()
+    }
+    SimulerInsertionPièce(pièce: Pièce): void {
+        this._decorated.SimulerInsertionPièce(pièce);
+    }
+    CountInvocationsMakeACoffee(){
+        return this._decorated.CountInvocationsMakeACoffee();
+    }
+}
+
+export class CaféLimitéeDecorator extends HardwareFakeDecorator {
+    private stock: number;
+    public constructor(decorated: HardwareFakeInterface, limite: number) {
+        super(decorated);
+        this.stock = limite
+    }
+
+    PourWater(): boolean {
+        if (this.stock > 0) {
+            this.stock--;
+            return true;
+        }
+        return false;
+    }
+
+    TryPullWater(): boolean {
+        if (this.stock > 0) {
+            this.stock--;
+            return true;
+        }
+        return false;
+    }
+}
---
+++ test/utilities/PénurieCaféDecorator.ts
@@ -0,0 +1,61 @@
+import {ButtonCodes} from "../../src/hardware/hardware.interface";
+import {HardwareFake} from "./HardwareFake";
+
+export class HardwareFakeDecorator extends HardwareFake {
+    private _decorated: HardwareFake;
+
+    public constructor(decorated: HardwareFake) {
+        super();
+        this._decorated = decorated;
+    }
+
+    RegisterMoneyInsertedCallback(callback: (coinValue: number) => void): void {
+        this._decorated.RegisterMoneyInsertedCallback(callback)
+    }
+    FlushStoredMoney(): void {
+        this._decorated.FlushStoredMoney()
+    }
+    CollectStoredMoney(): void {
+        this._decorated.CollectStoredMoney()
+    }
+    IsCupPresent(): boolean {
+        return this._decorated.IsCupPresent()
+    }
+    ProvideCup(): void {
+        return this._decorated.ProvideCup()
+    }
+    RegisterButtonPressedCallback(callback: (buttonCode: ButtonCodes) => void): void {
+        return this._decorated.RegisterButtonPressedCallback(callback)
+    }
+    MakeACoffee(): boolean {
+        return this._decorated.MakeACoffee()
+    }
+    TryPullWater(): boolean {
+        return this._decorated.TryPullWater()
+    }
+    PourMilk(): boolean {
+        return this._decorated.PourMilk()
+    }
+    PourWater(): boolean {
+        return this._decorated.PourWater()
+    }
+    PourSugar(): boolean {
+        return this._decorated.PourSugar()
+    }
+}
+
+export class PénurieCaféDecorator extends HardwareFakeDecorator {
+    private stock: number;
+    public constructor(decorated: HardwareFake, limite: number) {
+        super(decorated);
+        this.stock = limite
+    }
+
+    PourWater(): boolean {
+        return false
+    }
+
+    TryPullWater(): boolean {
+        return false
+    }
+}
